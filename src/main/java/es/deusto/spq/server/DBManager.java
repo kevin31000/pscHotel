43f21a0ba8ffca0928c04ad556ca025b61628ff6
kevin31000 <kevin.arnaiz@opendeusto.es>
2021-03-26 13:10:36 +0100

    DB manager update métodos Cliente

--- src/main/java/es/deusto/spq/server/DBManager.java
+++ src/main/java/es/deusto/spq/server/DBManager.java
@@ -3,6 +3,10 @@ package es.deusto.spq.server;
 import org.glassfish.grizzly.http.server.HttpServer;
 import javax.jdo.PersistenceManager;
 import javax.jdo.PersistenceManagerFactory;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.jdo.Extent;
 import javax.jdo.Query;
 import javax.jdo.JDOHelper;
@@ -30,6 +34,121 @@ public class DBManager {
 //		}
 
 		return instance;
+	}
+
+	public void store(Cliente client) {
+		DBManager.getInstance().storeObjectInDB(client);
+	}
+
+	public void delete(Cliente client) {
+		DBManager.getInstance().deleteObjectFromDB(client);
+	}
+
+	public void update(Cliente client) {
+		Cliente user2 = getUsuario(client.getEmail());
+		if (user2 != null) {
+			System.out.println(user2.getNombre() + user2.getApellidos() + user2.getEmail());
+			delete(user2);
+			store(client);
+		}
+	}
+
+	public Cliente getUsuario(String email) {
+		PersistenceManager pm = pmf.getPersistenceManager();
+		pm.getFetchPlan().setMaxFetchDepth(4);
+		Transaction tx = pm.currentTransaction();
+		Cliente user = null;
+
+		try {
+			tx.begin();
+
+			Query<?> query = pm.newQuery("SELECT FROM " + Cliente.class.getName() + " WHERE email == '" + email + "'");
+			query.setUnique(true);
+			user = (Cliente) query.execute();
+
+			tx.commit();
+		} catch (Exception ex) {
+			System.out.println(" $ Error cogiendo el usuario de la BD: " + ex.getMessage());
+		} finally {
+			if (tx != null && tx.isActive()) {
+				tx.rollback();
+			}
+
+			pm.close();
+		}
+
+		return user;
+	}
+
+	public List<Cliente> getClientes() {
+		List<Cliente> usuarios = new ArrayList<Cliente>();
+		PersistenceManager pm = pmf.getPersistenceManager();
+		pm.getFetchPlan().setMaxFetchDepth(4);
+		Transaction tx = pm.currentTransaction();
+
+		try {
+
+			tx.begin();
+
+			Extent<Cliente> extent = pm.getExtent(Cliente.class, true);
+
+			for (Cliente usuario : extent) {
+				usuarios.add(usuario);
+			}
+
+			tx.commit();
+		} catch (Exception ex) {
+			System.out.println("  $ Error retrieving all the Categories: " + ex.getMessage());
+		} finally {
+			if (tx != null && tx.isActive()) {
+				tx.rollback();
+			}
+
+			pm.close();
+		}
+
+		return usuarios;
+	}
+
+	public void deleteClientes() {
+		List<Cliente> usuarios = new ArrayList<Cliente>();
+		PersistenceManager pm = pmf.getPersistenceManager();
+		pm.getFetchPlan().setMaxFetchDepth(4);
+		Transaction tx = pm.currentTransaction();
+
+		try {
+
+			tx.begin();
+
+			Extent<Cliente> extent = pm.getExtent(Cliente.class, true);
+
+			for (Cliente usuario : extent) {
+				pm.deletePersistent(usuario);
+			}
+
+			tx.commit();
+		} catch (Exception ex) {
+			System.out.println("  $ Error retrieving all the Categories: " + ex.getMessage());
+		} finally {
+			if (tx != null && tx.isActive()) {
+				tx.rollback();
+			}
+
+			pm.close();
+		}
+
+	}
+
+	public void initializeData() {
+		System.out.println(" * Initializing data base");
+		// Cliente c1 = new client...
+		try {
+			// store(c1);
+		} catch (Exception ex) {
+			// TODO: handle exception
+			System.out.println(" $ Error initializing data: " + ex.getMessage());
+			ex.printStackTrace();
+		}
 	}
 
 }
