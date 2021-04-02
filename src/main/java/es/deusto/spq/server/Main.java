e1bb9ea3ea7e95f8d401800fb61bec9989bc130e
kevin31000 <kevin.arnaiz@opendeusto.es>
2021-04-02 12:41:15 +0200

    Ajustes parte servidora

--- src/main/java/es/deusto/spq/server/Main.java
+++ src/main/java/es/deusto/spq/server/Main.java
@@ -13,7 +13,7 @@ import java.net.URI;
  */
 public class Main {
     // Base URI the Grizzly HTTP server will listen on
-    public static final String BASE_URI = "http://localhost:8080/myapp/";
+    //public static final String BASE_URI = "http://localhost:8080/myapp/";
 
     /**
      * Starts Grizzly HTTP server exposing JAX-RS resources defined in this application.
@@ -26,18 +26,34 @@ public class Main {
 
         // create and start a new instance of grizzly http server
         // exposing the Jersey application at BASE_URI
-        return GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), rc);
+        return GrizzlyHttpServerFactory.createHttpServer(URI.create(getURL()), rc);
     }
+    
+    public static String getURL(){
+    	GetProperties properties = new GetProperties();
+		String url = "";
+		
+		try {
+			url = properties.getURL();
+		} catch (IOException e2) {
+			// TODO Auto-generated catch block
+			e2.printStackTrace();
+		}
+		return url;
+		
+	}
 
     /**
      * Main method.
      * @param args
      * @throws IOException
      */
+    @SuppressWarnings("deprecation")
     public static void main(String[] args) throws IOException {
+    	DBManager.getInstance();
         final HttpServer server = startServer();
         System.out.println(String.format("Jersey app started with WADL available at "
-                + "%sapplication.wadl\nHit enter to stop it...", BASE_URI));
+                + "%sapplication.wadl\nHit enter to stop it...", getURL()));
         System.in.read();
         server.stop();
     }
