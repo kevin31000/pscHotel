a9b15c83765e367954df6f1e40b750fd077a7a9c
kevin31000 <kevin.arnaiz@opendeusto.es>
2021-04-18 19:03:45 +0200

    ControllerTest

---
+++ src/test/java/es/deusto/spq/client/ControllerTest.java
@@ -0,0 +1,37 @@
+package es.deusto.spq.client;
+
+import static org.junit.Assert.assertEquals;
+
+import org.databene.contiperf.junit.ContiPerfRule;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+public class ControllerTest {
+	
+	ServiceLocator servicelocator;
+	Controller controller;
+	Habitacion habitacion;
+	
+	@Before
+    public void setUp(){
+		servicelocator = new ServiceLocator();
+		controller = new Controller(servicelocator);
+	}
+	
+	@Test
+    public void iniciarSesionTest(){	
+		assertEquals(controller.iniciarSesion("test@test.es", "test"), 1);
+	}
+	
+	@Test
+    public void registrarUsuarioTest(){	
+		assertEquals(controller.anadirCliente("a", "a", "a", "a", "a", false), true);
+	}
+	
+	/*@Test
+    public void getHabitacionTest(){	
+		assertEquals(controller.getHabitacion("H1").getCodigo(), "H1");
+	}*/
+
+}
