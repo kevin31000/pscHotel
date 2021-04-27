24f287255b6e95df2e4f55a7e705a775065b52a3
MikelCastroNieva <mikel.castro@opendeusto.es>
2021-04-27 21:25:06 +0200

    Clase ventana mostrar reservas

---
+++ src/main/java/es/deusto/spq/client/gui/VentanaMostrarReservas.java
@@ -0,0 +1,14 @@
+package es.deusto.spq.client.gui;
+
+import javax.swing.JFrame;
+
+import es.deusto.spq.client.Controller;
+import es.deusto.spq.client.Reserva;
+
+public class VentanaMostrarReservas extends JFrame{
+	
+	public VentanaMostrarReservas(Controller controller, Reserva reserva)
+	{
+		
+	}
+}
