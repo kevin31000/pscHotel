3a871a663e5410370cfea9de6874c16383ff3290
Sergio <sergi.lopez@opendeusto.es>
2021-04-27 22:39:08 +0200

    Varios test de la clase controller

--- src/test/java/es/deusto/spq/client/ControllerTest.java
+++ src/test/java/es/deusto/spq/client/ControllerTest.java
@@ -1,17 +1,26 @@
 package es.deusto.spq.client;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.util.List;
 
 import org.databene.contiperf.junit.ContiPerfRule;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
+
 
 public class ControllerTest {
 	
 	ServiceLocator servicelocator;
 	Controller controller;
 	Habitacion habitacion;
+	private static Cliente c = new Cliente("123", "nombre", "apellido", "email", "contrasenya", true);
+	
+	@Rule 
+	public ContiPerfRule i = new ContiPerfRule();
 	
 	@Before
     public void setUp(){
@@ -19,19 +28,56 @@ public class ControllerTest {
 		controller = new Controller(servicelocator);
 	}
 	
-	/*@Test
+	@Test
     public void iniciarSesionTest(){	
 		assertEquals(controller.iniciarSesion("test@test.es", "test"), 1);
 	}
 	
 	@Test
     public void registrarUsuarioTest(){	
-		assertEquals(controller.anadirCliente("a", "a", "a", "a", "a", false), true);
+		String DNI_usuario = c.getDNI();
+		String nombre_usuario = c.getNombre();
+		String apellido_usuario = c.getApellido();
+		String email_usuario = c.getEmail();
+		String contrasenya_usuario = c.getContrasenya();
+		controller.anadirCliente(DNI_usuario, nombre_usuario, apellido_usuario, email_usuario, contrasenya_usuario, false);
+		
+	}
+	
+//	@Test
+//    public void getHabitacionTest(){	
+//		assertEquals(controller.getHabitacion("H1").getCodigo(), "H1");
+//	}
+	
+	@Test
+	public void obtenerHabitaciones() {
+		List<Habitacion> habitaciones = controller.obtenerHabitaciones();
+		for (Habitacion habitacion : habitaciones) {
+			Assert.assertNotNull(habitacion);
+			
+		}
+	}
+	
+	
+	@Test
+	public void obtenerClientes() {
+		List<Cliente> clientes = controller.obtenerClientes();
+		for (Cliente cliente : clientes) {
+			Assert.assertNotNull(cliente);
+			
+		}
 	}
 	
 	@Test
-    public void getHabitacionTest(){	
-		assertEquals(controller.getHabitacion("H1").getCodigo(), "H1");
-	}*/
+	public void obtenerReservas() {
+		List<Reserva> reservas = controller.obtenerReservas();
+		for (Reserva reserva : reservas) {
+			Assert.assertNotNull(reserva);
+			
+		}
+	}
+	
+	
+	
 
 }
